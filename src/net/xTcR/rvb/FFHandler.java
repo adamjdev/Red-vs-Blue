e86c136813979628a4fd2c50da8016b53d16b3b8
TekkitCommando <admin@xTcR.net>
2015-03-24 22:11:05 -0500

    Added important initializers

---
+++ src/net/xTcR/rvb/FFHandler.java
@@ -0,0 +1,5 @@
+package net.xTcR.rvb;
+
+public class FFHandler {
+
+}
--- src/net/xTcR/rvb/RvB.java
+++ src/net/xTcR/rvb/RvB.java
@@ -1,5 +1,7 @@
+// net.xtcr is the default package for all xTcR minigames
 package net.xTcR.rvb;
 
+// these are the classed we need imported for RvB to work
 import org.bukkit.plugin.java.*;
 
 import java.util.ArrayList;
@@ -12,8 +14,11 @@ import org.bukkit.command.*;
 import org.bukkit.entity.*;
 import org.bukkit.*;
 
+// this is where the plugin will extend JavaPlugin
 public class RvB extends JavaPlugin
 {
+	/* this initialized the Strings, ArrayLists, LinkedLists,
+    int's, booleans, and HashMaps for RvB */
     public final Logger logger;
     public static RvB plugin;
     private final String PREFIX = ChatColor.RED + "RvB";
@@ -32,21 +37,70 @@ public class RvB extends JavaPlugin
     public static boolean unlimitedScore;
     public static boolean isTeamDeathmatch;
     public static boolean isDeathmatch;
+    public static final FFHandler ff_handler = new FFHandler();
+    public static final DPRHandler dpr_handler = new DPRHandler();
+    public static final DPMHandler dpm_handler = new DPMHandler();
+    public static final DMHandler dm_handler = new DMHandler();
     
-    
+    // this will initialize the logger
     public RvB() {
         this.logger = Logger.getLogger("Minecraft");
     }
     
+    // this is what will happen when the server is stopped
     public void onDisable() {
         final PluginDescriptionFile pdfFile = this.getDescription();
         this.logger.info(String.valueOf(pdfFile.getName()) + " Has Been Disabled!");
     }
     
+    // This is what will happen when the server is started
     public void onEnable() {
         final PluginDescriptionFile pdfFile = this.getDescription();
         this.logger.info(String.valueOf(pdfFile.getName()) + " Has Been Enabled!");
+        
+        red = new LinkedList<Entity>();
+        blue = new LinkedList<Entity>();
+        
+        dead = new ArrayList<Entity>();
+        respawn_left = new ArrayList<Integer>();
+        
+        scores = new HashMap<Player, Integer>();
+        deaths = new HashMap<Player, Integer>();
+        
+        isDeathmatch = false;
+        
+        System.out.println("[Red vs Blue] Loading config...");
+        
+        // loadConfig() will produce an error till later
+        loadConfig();
+        
+        System.out.println("Red vs Blue] Config loaded!");
+        
+		respawn_time = getConfig().getInt("Respawn.time");
+
+		System.out.println("[WarFighter] Setting timelimit...");
+
+		timeLimit = getConfig().getInt("General.timelimit") / 60;
+
+		time = getConfig().getInt("General.timelimit");
+
+		System.out.println("Loading Time Limit...");
+
+		unlimitedTime = getConfig().getBoolean("General.unlimtedtime");
+
+		System.out.println("Loading Time Preferences...");
+
+		unlimitedScore = getConfig().getBoolean("General.unlimtedscore");
+
+		System.out.println("Loading Score Preferences...");
+
+		System.out.println(timeLimit + "minutes");
+
+		System.out.println("[Red vs Blue] Registering events...");
+		
+		// Commands will be loaded here
     }
+    // This command wont matter in the future
     public final String Game_Version = "1.0";
     
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
